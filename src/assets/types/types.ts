4a78d9a0d6cc6c86e4146f26ddf2e53a6e39ae5d
Lukasz Pelczar <lukasxxz@o2.pl>
2021-08-05 12:16:34 +0200

    add video normalized data types

---
+++ src/assets/types/types.ts
@@ -0,0 +1,14 @@
+export type normalizeVideoDataType = {
+  id: string,
+  title: string,
+  description: string,
+  publishDate: string,
+  thumbnail: string,
+  likeCount: string,
+  viewCount: string,
+}
+
+export type videoDataType = {
+  API: string,
+  VIDEO: normalizeVideoDataType,
+}
